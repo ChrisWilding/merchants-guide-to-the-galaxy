c82f3462c235b99b98fafb8bbd2e015e9f777674
Chris Wilding <hello@chriswilding.co.uk>
2017-03-09 15:44:07 +0000

    Update the galatic to arabic service to wrap the how much parser

--- lib/merchant/services/galatic_to_arabic_service.rb
+++ lib/merchant/services/galatic_to_arabic_service.rb
@@ -1,13 +1,24 @@
 module Merchant
   class GalaticToArabicService
-    def initialize(translator)
+    def initialize(translator, parser = HowMuchParser.new)
       @converter = RomanToArabicConverter.new
+      @parser = parser
       @translator = translator
     end
 
-    def convert(numeral)
-      translated = @translator.translate_numerals(numeral)
+    def convert(numerals)
+      translated = @translator.translate_numerals(numerals)
       @converter.convert(translated)
+    end
+
+    def handles?(str)
+      @parser.can_parse?(str)
+    end
+
+    def process(str)
+      numerals = @parser.parse(str)
+      converted = convert(numerals)
+      "pish tegj glob glob is #{converted}"
     end
   end
 end
--- spec/services/galatic_to_arabic_service_spec.rb
+++ spec/services/galatic_to_arabic_service_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe Merchant::GalaticToArabicService do
   it 'converts galatic numerals to arabic' do
     expect(subject.convert('tegj pish prok glob')).to eq(66)
   end
+
+  it 'process returns how much the galatic numeral is' do
+    result = subject.process('how much is pish tegj glob glob ?')
+    expect(result).to eq('pish tegj glob glob is 42')
+  end
+
+  context 'when the parser can parse the string' do
+    let(:parser) { double('parser') }
+    it 'handles? method returns true' do
+      allow(parser).to receive(:can_parse?).and_return(true)
+      service = described_class.new(nil, parser)
+      expect(service.handles?('FAKE')).to be_truthy
+    end
+  end
+
+  context 'when the parser can not parse the string' do
+    let(:parser) { double('parser') }
+    it 'handles? method returns true' do
+      allow(parser).to receive(:can_parse?).and_return(false)
+      subject = described_class.new(nil, parser)
+      expect(subject.handles?('FAKE')).to be_falsey
+    end
+  end
 end
