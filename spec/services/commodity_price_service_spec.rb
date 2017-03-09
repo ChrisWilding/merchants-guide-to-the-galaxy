62dbb4ca2147cca6c5f50869b165443d8a8b70f0
Chris Wilding <hello@chriswilding.co.uk>
2017-03-09 23:49:06 +0000

    Refactor commodity price service specs

--- spec/services/commodity_price_service_spec.rb
+++ spec/services/commodity_price_service_spec.rb
@@ -1,29 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe Merchant::CommodityPriceService do
-  context 'when the parser can parse the string' do
-    it 'handles? method returns true' do
-      parser = double('parser')
-      allow(parser).to receive(:can_parse?).and_return(true)
-      service = described_class.new(nil, nil, parser)
-      expect(service.handles?('FAKE')).to be_truthy
-    end
-  end
-
-  context 'when the parser can not parse the string' do
-    it 'handles? method returns true' do
-      parser = double('parser')
-      allow(parser).to receive(:can_parse?).and_return(false)
-      subject = described_class.new(nil, nil, parser)
-      expect(subject.handles?('FAKE')).to be_falsey
-    end
-  end
-
   subject do
-    translator = Merchant::GalaticToRomanService.new
-    translator.process('glob is I')
-    translator.process('prok is V')
-    galatic_to_arabic_service = Merchant::GalaticToArabicService.new(translator)
+    galatic_to_roman_service = Merchant::GalaticToRomanService.new
+    galatic_to_roman_service.process('glob is I')
+    galatic_to_roman_service.process('prok is V')
+    galatic_to_arabic_service = Merchant::GalaticToArabicService.new(galatic_to_roman_service)
     price_definition_service = Merchant::PriceDefinitionService.new(galatic_to_arabic_service)
     price_definition_service.process('glob glob Silver is 34 Credits')
     described_class.new(galatic_to_arabic_service, price_definition_service)
@@ -32,5 +14,20 @@ RSpec.describe Merchant::CommodityPriceService do
   it 'process calculates the unit price in credits for a commodity' do
     result = subject.process('how many Credits is glob prok Silver ?')
     expect(result).to eq('glob prok Silver is 68 Credits')
+  end
+
+  context 'handles?' do
+    let(:parser) { double('parser') }
+    subject { described_class.new(nil, nil, parser) }
+
+    it 'returns true when the parser can parse' do
+      allow(parser).to receive(:can_parse?).and_return(true)
+      expect(subject.handles?('FAKE')).to be_truthy
+    end
+
+    it 'return false when the parser can not parse' do
+      allow(parser).to receive(:can_parse?).and_return(false)
+      expect(subject.handles?('FAKE')).to be_falsey
+    end
   end
 end
